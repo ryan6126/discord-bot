18d31b8f14cc54fef2325bfb8ba8fd7bc49bf60d
ryan6126 <67865238+ryan6126@users.noreply.github.com>
2020-08-09 21:36:41 +0200

    Update restart.js

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "restart",
     category: "owner",
     run: async (client, message, args) => {
-        if (message.author.id !== '618266942288953345') {
+        if (message.author.id !== 'OWNER ID') {
             return;
         }
         await message.channel.send(`Im out.`)
